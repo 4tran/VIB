cee049d84364610324419e76c26cf8de3dd94331
QuakersOat <quakersoat@gmail.com>
2016-05-18 11:36:39 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,3 @@
 <?php
-$config = json_decode(file_get_contents("../config.json"), true);
-$db = new mysqli($config['ip'], $config['user'], $config['pass'], $config['db']);
-
-if ($db->connect_errno) {
-        echo "Failed to connect to database. Please check config.json";
-}
-else {
-        echo "Successfully connected to the database!";
-}
+require("../res/index.php");
 ?>
