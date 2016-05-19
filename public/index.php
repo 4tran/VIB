8ebe786d92fc4e236df3a3286b6b7c0e32f0cdbc
QuakersOat <quakersoat@gmail.com>
2016-05-19 14:01:26 -0500

    fix

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 require("../res/config.php");
-$db->real_query("SELECT * FROM posts ORDER BY id ASC");
+$db->real_query("SELECT * FROM posts ORDER BY id DESC LIMIT 5");
 $res = $db->use_result();
 echo "<head><title> Home - " . $config['site_name'] . "</title></head>";
-echo "Here's the latest post: <br/>";
+echo "Here's the latest posts: <br/>";
 while ($row = $res->fetch_assoc()) {
   echo "#" . $row['id'] . ": " . $row['content'] . "<br/>";
 }
