690db3a25755123827f10f26b70b18311188923c
QuakersOat <quakersoat@gmail.com>
2016-05-19 13:53:51 -0500

    more relevant

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 require("../res/config.php");
-$db->real_query("SELECT * FROM fruits ORDER BY id ASC");
+$db->real_query("SELECT * FROM posts ORDER BY id ASC");
 $res = $db->use_result();
 echo "<head><title> Home - " . $config['site_name'] . "</title></head>";
-echo "Here's a list of my favorite fruits: <br/>";
+echo "Here's the latest post: <br/>";
 while ($row = $res->fetch_assoc()) {
-  echo "#" . $row['id'] . ": " . $row['fruit'] . "<br/>";
+  echo "#" . $row['id'] . ": " . $row['content'] . "<br/>";
 }
 ?>
