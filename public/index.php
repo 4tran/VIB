7fe39d8eb27b88022575e90fd2f4a5cc61b84fb7
QuakersOat <quakersoat@gmail.com>
2016-05-19 08:10:40 -0500

    figured out database connection

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
-require("../res/index.php");
+require("../res/config.php");
+$db->real_query("SELECT * FROM fruits ORDER BY id ASC");
+$res = $db->use_result();
+echo "<head><title> Home - " . $config['site_name'] . "</title></head>";
+echo "Here's a list of my favorite fruits: <br/>";
+while ($row = $res->fetch_assoc()) {
+    echo "#" . $row['id'] . ": " . $row['fruit'] . "<br/>";
+}
 ?>
---
+++ res/config.php
@@ -0,0 +1,7 @@
+<?php
+$config = json_decode(file_get_contents("../config.json"), true);
+$db = new mysqli($config['ip'], $config['user'], $config['pass'], $config['db']$
+if ($db->connect_errno) {
+        echo "Failed to connect to database. Please check config.json";
+}
+?>
