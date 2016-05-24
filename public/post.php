8eb6560801c88971ecc41613412aa58d536a79c3
QuakersOat <quakersoat@gmail.com>
2016-05-24 09:40:01 -0500

    op posting ACTUALLY works now

--- public/post.php
+++ public/post.php
@@ -3,8 +3,18 @@ require("../res/config.php");
 $url = $db->real_escape_string($_POST["url"]);
 $name = $db->real_escape_string($_POST["name"]);
 $content = $db->real_escape_string($_POST["content"]);
-$db->query("INSERT INTO posts_".$url." (name, content, timestamp)
-VALUES ('$name', '$content', now())");
+
+$db->real_query("SELECT id FROM posts_".$url." ORDER BY id DESC LIMIT 1");
+$id = 0;
+$res = $db->use_result();
+while ($row = $res->fetch_assoc()) {
+  $id = $row['id'];
+}
+$id++;
+$id = $db->real_escape_string($id);
+
+$db->query("INSERT INTO posts_".$url." (name, content, op, timestamp)
+VALUES ('$name', '$content', '$id', now())");
 $url = $_POST['url'];
 echo "<a href=\"$url\">Click here to go back to the board home.</a>";
 ?>
