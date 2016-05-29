015d042559d4a00c0d1949808f5de9f787ec83f8
QuakersOat <quakersoat@gmail.com>
2016-05-29 15:57:39 -0500

    greentexting and linking complete

--- public/index.php
+++ public/index.php
@@ -23,20 +23,12 @@ while ($row = $res->fetch_assoc()) {
 }
 echo "<div class=\"recent\">";
 echo "<h3 id=\"latest_posts\">Latest Posts</h3><br/>";
-$re = "/^^(>[a-zA-Z0-9_ \.\,\+\=\-\~\`\!\@\#\$\%\^\&\*\(\)\{\}\[\]\'\;\:\?\|\"\/\\\<\!]*$)/mi";
-$subst = "<p class=\"quote\">$1</p>";
-$re1 = "/^^(>>(\\d+))/mi";
 foreach ($boards as $x) {
-  $subst1 = "<a href=\"$x/$2\">$1</a>";
   $x = $db->real_escape_string($x);
   $db->real_query("SELECT * FROM posts_".$x." ORDER BY timestamp DESC LIMIT 1");
   $res = $db->use_result();
   while ($row = $res->fetch_assoc()) {
-    $str = str_replace("\r\n", "\n", $row['content']);
-    $str = str_replace("\r", "\n", $str);
-    $content = preg_replace($re, $subst, $str);
-    $content = preg_replace($re1, $subst1, $content);
-    echo "<p style=\"font-size:110%;\">/$x/ - </p>" . "<p>" . nl2br($content) . "<a href=\"" . "$x/" . $row['op'] . "\"> [reply]</a></p><br/>";
+    echo "<p style=\"font-size:110%;\">/$x/ - </p>" . "<p>" . nl2br($row["content"]) . "<a href=\"" . "$x/" . $row['op'] . "\"> [reply]</a></p><br/>";
   }
 }
 echo "</div>";
